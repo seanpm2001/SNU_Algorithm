65534c4f2cb2efec3208c6429124dee3052a82d8
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-01 20:54:05 -0700

    2 JavaScript OS Object list files
    2 JavaScript operating system list files. The original version, plus an archive of its first version.

---
+++ Algorithm/JSOBJECTS/OperatingSystemObjectList.js
@@ -0,0 +1,16 @@
+/* JavaScript object list library
+* Operating systems scripts
+*/
+// Start of OS list
+var amigaOS = {type="OperatingSystem" model: "20" color: "Grey/Gray, White, Black, Red", fileSystems: "" firstVer: "", lastVer: "", successor: "", kernelType: "" developer: "Amiga", };
+var UNIXOS = {type="OperatingSystem", model: "6" color: "Grey/Gray, White, Black", fileSystems: " ", firstVer: "", lastVer: " ", successor: " ", kernelType: "Monolithic" developer: "Bell Labs", };
+var appleSystemOS = {type: "OperatingSystem", model: "59", color: "Grey/Gray, White, Black", fileSystems: "HFS, HFS+", firstVer: "Apple System 1", lastVer: "Apple System 7" sucessor: "MacOS_Classic", kernelType: "Monolithic", developer: "Apple Computer Company"};
+var windowsOS = {type: "OperatingSystem", model: "60", color:"Red, Green, Blue Yellow", fileSystems: "FAT8, FAT16, FAT32, NTFS, GPT", firstVer: "Windows 1.0" kernelType: "Unknown" developer: "Microsoft"};
+// End of OS list
+/* File info
+File type: *.js (JavaScript file)
+File version: 1 (November 3rd 2019)
+First version: 1 (November 3rd 2019)
+Line count: 16
+Purpose: JavaScript object testing
+*/
